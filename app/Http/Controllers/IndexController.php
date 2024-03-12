8d7032afff3014de9393c6d8995d0e417727cd48
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2024-03-12 15:33:40 +0300

    sort past events by desc

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
     public function index()
     {
         $events = Event::orderby('date', 'asc')->paginate(3);
-        $pastevents = PastEvent::orderby('date', 'asc')->paginate(4);
+        $pastevents = PastEvent::orderby('date', 'desc')->paginate(4);
         foreach ($pastevents as $pastevent) {
             $date = \DateTime::createFromFormat('Y-m-d', $pastevent->date);
             $pastevent->date = $date->format('F j, Y');
